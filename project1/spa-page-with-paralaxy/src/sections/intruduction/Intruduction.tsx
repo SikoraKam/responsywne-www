a54e5411fe93cdb797f6f8a7f9a3bbff29dd9dc6
SikoraKam <sikorakam3@gmail.com>
2023-01-06 19:30:29 +0100

    project1: filled first section

--- project1/spa-page-with-paralaxy/src/sections/intruduction/Intruduction.tsx
+++ project1/spa-page-with-paralaxy/src/sections/intruduction/Intruduction.tsx
@@ -3,7 +3,47 @@ import "./intruduction.style.css";
 import "../../App.style.css";
 import dog from "../../assets/border-collie.png";
 
+const tableContent = [
+  {
+    title: "Rejon/kraj pochodzenia:",
+    info: "Wielka Brytania",
+  },
+  {
+    title: "Klasyfikacja FCI:",
+    info: "Grupa 1, Sekcja 1, Owczarki",
+  },
+  {
+    title: "Temperament:",
+    info: "wytrwały, inteligentny, energiczny, grzeczny, posłuszny, uważny",
+  },
+  {
+    title: "Kolory:",
+    info: "biały i czarny, biały z czarnym i beżowym, biały i wątrobianym, rudym i beżowym, beżowy i biały",
+  },
+  {
+    title: "Waga:",
+    info: "Pies: 14–20 kg, Suka: 12–19 kg",
+  },
+  {
+    title: "Wysokość:",
+    info: "Pies: 48–56 cm, Suka: 46–53 cm",
+  },
+  {
+    title: "Długość życia:",
+    info: "od 10 do 17 lat",
+  },
+];
+
 export const Intruduction: FC = () => {
+  const renderTableContent = () =>
+    tableContent.map((item) => (
+      <li>
+        <span className="table-item-title">{item.title}</span>
+        <span className="table-item-info">{item.info}</span>
+        <div className="divider" />
+      </li>
+    ));
+
   return (
     <>
       <header>
@@ -13,46 +53,33 @@ export const Intruduction: FC = () => {
       </header>
 
       <section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id
-        turpis id turpis semper fermentum quis at ipsum. Vivamus tellus urna,
-        vulputate id elementum non, convallis eu purus. Fusce ut pharetra
-        sapien, sollicitudin tempor lorem. Nullam ipsum ante, interdum quis ante
-        vitae, ultrices finibus turpis. Sed a lorem eleifend erat pharetra
-        tincidunt quis sed lectus. Etiam ultricies, dui ac rhoncus efficitur,
-        nunc ipsum mattis lorem, vitae maximus nisl arcu et lacus. Cras mollis,
-        sapien nec congue efficitur, nisi arcu egestas ex, sed congue felis ex
-        vulputate dolor. Proin sed urna et eros placerat sodales. Cras pharetra
-        tellus sed posuere sagittis. Nulla viverra dui sed diam eleifend
-        scelerisque. Etiam dapibus nec turpis sed luctus. Nam commodo feugiat
-        lacus non tristique. Suspendisse in sem a purus efficitur blandit sed
-        sed ex. Pellentesque varius molestie mauris. Donec laoreet magna ut
-        massa pellentesque, sit amet convallis arcu sollicitudin. Morbi id lorem
-        ac lorem consectetur egestas. Ut imperdiet blandit sapien, in venenatis
-        orci dapibus eget. Integer elit risus, egestas non quam ac, accumsan
-        ornare leo. Ut vitae laoreet ante. Mauris dictum augue lectus, et
-        condimentum ante hendrerit non. Praesent massa risus, rhoncus vel erat
-        quis, laoreet tincidunt velit. Maecenas enim dui, vulputate nec auctor
-        quis, hendrerit sed ipsum. Vivamus porta quam lacus, eget laoreet magna
-        mollis sit amet. Phasellus vel nulla felis. Mauris facilisis in tortor
-        vitae pharetra. Pellentesque vulputate a augue a gravida. Suspendisse
-        sed lacus massa. Proin placerat ante ut blandit laoreet. In consequat
-        erat a ipsum finibus consequat. Integer semper nibh ligula, gravida
-        pharetra lorem tristique sit amet. Sed a ligula est. Aliquam erat
-        volutpat. Sed malesuada, nisl vitae tincidunt feugiat, lorem urna
-        ultrices enim, eu convallis enim lorem eget nunc. Maecenas a elementum
-        nulla, id consequat lacus. Maecenas tincidunt tincidunt mollis. Maecenas
-        libero nisi, iaculis id velit et, varius sodales ante. Curabitur
-        dignissim placerat odio a egestas. Fusce a leo ut felis pellentesque
-        lacinia. Nullam vel erat a tortor ultricies eleifend. Nunc vitae mattis
-        lorem. Suspendisse non condimentum leo. Aliquam metus sem, viverra quis
-        sem sit amet, laoreet ullamcorper libero. Mauris molestie sed urna at
-        gravida. Cras ut arcu et eros aliquam convallis eget ut purus. Mauris
-        sagittis turpis metus, sit amet gravida ipsum placerat ut. Pellentesque
-        congue vestibulum elit sed congue. Integer interdum id justo vitae
-        vestibulum. Duis laoreet ante vitae tortor tempus mollis. Fusce sit amet
-        congue sem. Fusce hendrerit ex id lacus pharetra accumsan. Nullam augue
-        mi, semper a interdum eget, mattis sit amet lorem. Morbi lobortis nisl
-        non risus pretium, eget elementum turpis tempor.
+        <div className="about">
+          Border collie to niezwykle popularna rasa ciesząca się
+          zainteresowaniem miłośników psów na całym świecie. Sympatyczne,
+          aktywne psiaki średniej wielkości stanowią doskonałego towarzysza.
+        </div>
+        <ul className="table">{renderTableContent()}</ul>
+        <h3>Historia rasy</h3>
+        Border collie to brytyjskie psy pasterskie, które miały za zadanie
+        wykonywać prace związane z wypasem owiec w każdych możliwych warunkach
+        atmosferycznych. Z tego też powodu, są to psy bardzo inteligentne oraz
+        zdolne do szybkiej nauki. Potrafią doskonale wpływać na zachowanie
+        innych zwierząt, zwłaszcza owiec. Wykorzystują do tego szczekanie, a
+        także sugestywne spojrzenie. Za ich bezpośrednich przodków uważa się
+        krzyżówkę miejscowych psów z rejonów Wielkiej Brytanii z psami, które
+        przybyły wraz z wikingami. Były to sporych rozmiarów czworonogi
+        wykorzystywane do ochrony bydła. Na pierwsze wzmianki o tych psach,
+        które kochają pracę, możemy już natrafić w zapiskach z roku 943.
+        Dokładny opis rasy pochodzi z 1573 roku z książki Johannesa Caina. Od
+        tego stulecia nieustannie rosła liczba tych psów pojawiających się w
+        zagrodach na terenach Wielkiej Brytanii. Na przestrzeni lat rasa
+        rozprzestrzeniła się po całej Europie i z czasem zaczęła trafiać także
+        do domów, gdzie owce i bydło nie występowały w ogóle. Z tego też powodu
+        niektóre psy zatraciły nieco swoje wrodzone instynkty. Co ciekawe, nazwę
+        rasy oficjalnie nadano dopiero w 1916 roku. Interesującym może okazać
+        się również fakt, iż „collie” odnosi się do owczarków szkockich,
+        natomiast „border” świadczy o pograniczu Szkocji i Anglii, czyli
+        obszaru, z którego wywodzi się rasa.
       </section>
     </>
   );
